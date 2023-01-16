c1dbecfd29fac6fe4caebf2949a7bd65e9b7e96c
Bogdan Teodoru <a.teodoru8@gmail.com>
2023-01-16 22:18:43 +0100

    Try this

--- ddapi/Controllers/MainController.cs
+++ ddapi/Controllers/MainController.cs
@@ -9,18 +9,33 @@ namespace ddapi.Controllers;
 public class MainController : Controller {
    [HttpGet]
    public async Task<String> GetAllOrders() {
-      var apiKey = "4sqmKzToVkUoTsVfze4X";
-      var apiBaseUrl = "https://pasd-webshop-api.onrender.com/";
+      using (var httpClient = new HttpClient()) {
 
-      string response = null!;
-      HttpClient client = new();
-      client.BaseAddress = new Uri(apiBaseUrl);
-      client.DefaultRequestHeaders.Accept.Clear();
-      client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-      client.DefaultRequestHeaders.Add("x-api-key", apiKey);
+         using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://pasd-webshop-api.onrender.com/api/order/")) {
+
+         request.Headers.TryAddWithoutValidation("accept", "application/json");
+         request.Headers.TryAddWithoutValidation("x-api-key", "4sqmKzToVkUoTsVfze4X"); 
+
+         var response = await httpClient.SendAsync(request);
+         return await response.Content.ReadAsStringAsync();
+         }
+      }
       
-      HttpResponseMessage res = await client.GetAsync("https://pasd-webshop-api.onrender.com/api/order/");
-      response = await res.Content.ReadAsStringAsync();
-      return response;
+   }
+
+   public async Task<String> PostOrder() {
+      using (var httpClient = new HttpClient()) {
+
+      using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://pasd-webshop-api.onrender.com/api/delivery/")) {
+         request.Headers.TryAddWithoutValidation("accept", "application/json");
+         request.Headers.TryAddWithoutValidation("x-api-key", "4sqmKzToVkUoTsVfze4X"); 
+
+         request.Content = new StringContent("{\n  \"price_in_cents\": 0,\n  \"expected_delivery_datetime\": \"2023-01-16T21:02:47.309Z\",\n  \"order_id\": 0\n}");
+         request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json"); 
+
+         var response = await httpClient.SendAsync(request);
+         return await response.Content.ReadAsStringAsync();
+      }
+   }
    }
 }
