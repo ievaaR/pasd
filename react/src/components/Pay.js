3fe236e7cf89b854e9eb24f69b49e8eb181b03e9
Ieva Randyte <randyteieva@gmail.com>
2023-01-18 13:57:07 +0100

    done2

--- react/src/components/Pay.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Pay = () => {
-    return (
-        <div>
-            <h3>no its not on the house</h3>
-        </div>
-    )
-}
--- react/src/components/Surprise.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Surprise = () => {
-    return (
-        <div>
-            <h3>this should be a pic of a cute capybara but idk how to do it yet </h3>
-        </div>
-    )
-}
